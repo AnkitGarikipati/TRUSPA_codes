e2b7365606fa945a33e3dfc142d9e7e0d7f2736f
AnkitGarikipati <66576900+AnkitGarikipati@users.noreply.github.com>
2021-06-09 14:49:16 -0400

    Update load_phantom.m

--- functions/load_phantom.m
+++ functions/load_phantom.m
@@ -5,15 +5,3 @@ function [phantom_image] = load_phantom(phantom_file_name)
 
 phantom_image = rgb2gray(imread(phantom_file_name));
 end
-
-
-% pxl_intensity = [64, 100, 150, 200, 255]
-% den_list = [1300, 1200, 1105, 1234, 5555];
-% den_map = zeros(size(phantom));
-% for i in pxl/den_list
-    % coords_i = phantom(phantom == i);
-    % den_map(coords_i) = pxl(i);
-    % 
-% for i=1:601
-    % for j=1:601
-        
